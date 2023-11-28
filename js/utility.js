f1cd772fcbf8905c255429a783b2c0595f9faedf
ShovanTheMan <shovan2192@gmail.com>
2023-11-28 15:46:06 +0600

    function added

--- js/utility.js
+++ js/utility.js
@@ -1,2 +1,20 @@
-const myUtility = 'Inside utility file';
-console.log('this is utility file');
+function getInputFieldValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldStringValue = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldStringValue);
+    inputField.value = '';
+
+    
+    return inputFieldValue;
+}
+function getTextElementValueById(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseFloat(textElementValueString);
+    return textElementValue;
+
+}
+function setTextElementValueById(elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,23 +1,4 @@
-function getInputFieldValueById(inputId){
-    const inputField = document.getElementById(inputId);
-    const inputFieldStringValue = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldStringValue);
-    inputField.value = '';
-    return inputFieldValue;
-}
 
-function getTextElementValueById(elementId){
-    const textElement = document.getElementById(elementId);
-    const textElementValueString = textElement.innerText;
-    const textElementValue = parseFloat(textElementValueString);
-    return textElementValue;
-
-}
-
-function setTextElementValueById(elementId, newValue){
-       const textElement = document.getElementById(elementId);
-       textElement.innerText = newValue;
-}
 
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount = getInputFieldValueById('withdraw-field');
